6f27765375d4d15b8451d9454d892e021b7a8dc7
vaskarsarma <vaskarsarma@gmail.com>
2017-08-13 16:08:55 +0400

    blogs

--- public/js/common.js
+++ public/js/common.js
@@ -1,10 +1,22 @@
 'use strict';
 $(function() {
+
+    var categorytype = "all";
+    var startindex = 0;
+    if (($('#nextsearindex').val() != undefined))
+        startindex = $('#nextsearindex').val();
+
     $(".loginBtn").on("click", () => {
         $(".modalLogin").removeClass("hidden");
         // run_waitMe("signUp");
         //signUp(servicePath);
     });
+
+    // $('#divImage').on("click", function() {
+    //     if (($('#nextsearindex').val() != undefined))
+    //         startindex = $('#nextsearindex').val();
+    //     GetBlogsInfo(startindex, categorytype);
+    // });
 
     limitBlogLength();
 
@@ -65,7 +77,57 @@ let limitBlogLength = () => {
         if (data.length > showlength) {
             var c = data.substring(0, showlength);
             $('.blogdata .limitblogdata')[index].innerHTML = "";
-            $('.blogdata .limitblogdata')[index].innerHTML = "<p>" + c + "</p>";
+            $('.blogdata .limitblogdata')[index].innerHTML = "<p>" + c + "...</p>";
         }
     });
+};
+
+let GetBlogsInfo = (startindex, categorytype) => {
+    run_waitMe("blogdata");
+    $.when(GetCompiledTemplate("blogsection"), GetBlogsByStartIndex(startindex, categorytype))
+        .done(function(template, json) {
+
+            var data = { "index": json.index, "blogs": json.blogs };
+            var compiledTemplate = Handlebars.compile(template);
+            var newhtml = compiledTemplate(data);
+
+            if (startindex == 0 && categorytype != "all")
+                $(".blogdata").html(newhtml);
+            else
+                $(".blogdata").append(newhtml);
+
+            limitBlogLength();
+
+            if ($('#nextsearindex').val() != undefined) {
+                var idx = $('#nextsearindex').val();
+                if (parseInt(data.index) + 1 > idx) {
+                    $('#nextsearindex').val(data.index);
+                    $("#divImage").removeClass("hidden");
+                    $("#divImage").addClass("show");
+                } else {
+                    $("#divImage").removeClass("show");
+                    $("#divImage").addClass("hidden");
+                }
+            }
+        });
+    stop_waitMe("blogdata");
+};
+
+let GetBlogsByStartIndex = (startindex, categorytype) => {
+    console.log("GetBlogsByStartIndex : startindex : " + startindex + ", Category : " + categorytype);
+    var d = $.Deferred();
+
+    $.ajax({
+            method: "post",
+            url: "/commonapi/data/blog/",
+            data: { "si": startindex, "ct": categorytype }
+        })
+        .done(function(jsonResult) {
+            d.resolve(jsonResult);
+        })
+        .fail(function() {
+            d.reject;
+        })
+        .always(function() {});
+    return d.promise();
 };
